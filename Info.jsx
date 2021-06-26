fc45552e8d331c8faf6346c22c12a13efd33b7e7
ammu487 <85435493+ammu487@users.noreply.github.com>
2021-06-26 19:22:37 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript andReact.js</h1>
+      <p>This a an amazing bootcamp taken by Shaurya Shinha .We covered everything from scratch including Javascript React.js,HTML</p>
+    </div>
+  );
+}
+
+export default Info; 
